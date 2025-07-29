6baf013169f3ca1a7ca104499d8425a9f751c402
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-07-29 22:22:24 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -29,3 +29,13 @@ document.getElementById('division').addEventListener('click', function() {
   const result = division(number1, number2);
   document.getElementById('calculation-result').textContent = result;
 });
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
