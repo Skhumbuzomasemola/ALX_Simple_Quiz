840466de9c32034a1def9f644e8706459dc12cd2
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-07-29 21:40:14 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,24 @@
+function checkAnswer() {
+    // Function body
+  let input = document.getElementById("answer").value;
+  let correctAnswer = "4";
+}
+function checkAnswer() {
+  const selectedOption = document.querySelector('input[name="quiz"]:checked');
+  const correctAnswer = "4";
+
+  if (!selectedOption) {
+    alert("Please select an answer.");
+    return;
+  }
+
+  if (selectedOption.value === correctAnswer) {
+    alert("Correct! Well done.");
+  } else {
+    alert("That's incorrect. Try again!.");
+  }
+}
+
+let button = document.getElementById("SubmitAnswer");
+button.addEventListener("click", checkAnswer);
 
