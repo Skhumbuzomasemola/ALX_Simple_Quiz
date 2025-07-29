918cf259831b20bbbf6e1edb48970d62ac21fa26
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-07-29 22:21:09 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,31 @@
+function subtraction(number1, number2) {
+    return number1 - number2;
+}
+document.getElementById('subtraction').addEventListener('click', function() {
+  const number1 = 
+    parseFloat(document.getElementById('number1').value);
+  const number2 = 
+    parseFloat(document.getElementById('number2').value);
+  const result = subtraction(number1, number2);
+  document.getElementById('calculation-result').textContent = result;
+});
 
+function multiplication(number1, number2) {
+    return number1 * number2;
+}
+document.getElementById('multiplication').addEventListener('click', function() {
+  const number1 = parseFloat(document.getElementById('number1').value);
+  const number2 = parseFloat(document.getElementById('number2').value);
+  const result = multiplication(number1, number2);
+  document.getElementById('calculation-result').textContent = result;
+});
+
+function division(number1, number2) {
+    return number1 / number2;
+}
+document.getElementById('division').addEventListener('click', function() {
+  const number1 = parseFloat(document.getElementById('number1').value);
+  const number2 = parseFloat(document.getElementById('number2').value);
+  const result = division(number1, number2);
+  document.getElementById('calculation-result').textContent = result;
+});
