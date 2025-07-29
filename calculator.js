84f7e43d15431ea81f20e901701e4bd29245c950
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-07-29 22:23:22 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -10,10 +10,10 @@ document.getElementById('subtraction').addEventListener('click', function() {
   document.getElementById('calculation-result').textContent = result;
 });
 
-function multiplication(number1, number2) {
+function multiply(number1, number2) {
     return number1 * number2;
 }
-document.getElementById('multiplication').addEventListener('click', function() {
+document.getElementById('multiply').addEventListener('click', function() {
   const number1 = parseFloat(document.getElementById('number1').value);
   const number2 = parseFloat(document.getElementById('number2').value);
   const result = multiplication(number1, number2);
